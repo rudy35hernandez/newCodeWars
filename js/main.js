5c5141bedcc012dce5405c59dca143ac4e65c587
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-01-10 00:07:53 -0800

    added challenge

--- js/main.js
+++ js/main.js
@@ -1848,7 +1848,11 @@ var isAnagram = function(test, original) {
 
 /*
 
-4.
+4. Instructions
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
 
 */
 
