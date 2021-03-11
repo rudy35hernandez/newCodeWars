938835293f0782cfd36a3bd7349a4584f8c5e453
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-10 23:59:29 -0800

    Added another challenge

--- js/main.js
+++ js/main.js
@@ -219,4 +219,18 @@ Return True if yes, False otherwise :)
 function hero(bullets, dragons){
     return bullets >= dragons * 2 ? true : false
  }
+
+ /*
+
+ 15. Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+SQL: return results in a column named greeting
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+*/
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+  }
  
