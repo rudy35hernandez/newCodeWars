90af188fefd0db9f58168bac1c8119b13cc53524
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-11 15:40:16 -0800

    added a challenge

--- js/main.js
+++ js/main.js
@@ -268,4 +268,10 @@ function sum (numbers) {
 
   /* 
 
-  17. 
+  17. This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+  */
+
+    var a = "code"
+    var b = "wa.rs"
+    var name = a + b;
