041450406fdad56559ef85bcb9170c11fac113eb
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-11 16:04:03 -0800

    added another 1

--- js/main.js
+++ js/main.js
@@ -282,4 +282,16 @@ function sum (numbers) {
     18. How do you freeze an object/class to prevent changes? Lets name this class MrFreeze
     */
 
-    Object.freeze(MrFreeze)
+    Object.freeze(MrFreeze);
+
+
+    /*
+    
+    19. Complete the solution so that it reverses all of the words within the string passed in.
+
+    */
+
+    function reverseWords(str){
+  
+        return str.split(/\s+/).reverse().join(' ')
+    }
