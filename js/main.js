93428f326e48e5bd6b7bf3ccdd98f37e7ef5611b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-11 17:51:37 -0800

    Another 1 DJ KHALED VOICE

--- js/main.js
+++ js/main.js
@@ -295,3 +295,17 @@ function sum (numbers) {
   
         return str.split(/\s+/).reverse().join(' ')
     }
+
+
+    /* 
+
+    20. I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
+     I'll appreciate for your help.
+
+    P.S. Each array includes only integer numbers. Output is a number too.
+
+    */
+
+    function arrayPlusArray(arr1, arr2) {
+        return arr1.reduce((sum, num) => sum + num, 0) + arr2.reduce((sum, num) => sum + num, 0); //something went wrong
+      }
