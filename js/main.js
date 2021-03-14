3538265ee3b7369f0ce056d4ceaf913e64cfdf2c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-13 22:56:52 -0800

    new challenge today

--- js/main.js
+++ js/main.js
@@ -341,3 +341,17 @@ function sum (numbers) {
 
         return name[0] == 'r' || arrayName[0] == 'R' ? name + ' plays banjo' : name + ' does not play banjo'
       }
+
+    
+
+    /*
+
+    23. Write a function which calculates the average of the numbers in a given list.
+
+    Note: Empty arrays should return 0
+
+    */
+
+    function find_average(array) {
+        return array.reduce((sum, num) => sum + num,0) / array.length;
+      }
