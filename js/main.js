8fa6ab52108b264d5e85d13260a4ce45c99949f0
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-13 22:58:19 -0800

    another 1?

--- js/main.js
+++ js/main.js
@@ -355,3 +355,11 @@ function sum (numbers) {
     function find_average(array) {
         return array.reduce((sum, num) => sum + num,0) / array.length;
       }
+
+
+    
+    /*
+
+    24.
+
+    */
