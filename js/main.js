3933df59c385809319b396fc6b1ec2e1bc8787a6
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-14 23:06:23 -0700

    getting max num and min num in an array!

--- js/main.js
+++ js/main.js
@@ -376,6 +376,18 @@ function sum (numbers) {
 
     /* 
 
-    25.
+    25. Your task is to make two functions, max and min (maximum and minimum in PHP and Python) that take a(n) 
+    array/vector of integers list as input and outputs, respectively, the largest and lowest number in that 
+    array/vector.
 
     */ 
+
+    var min = function(list){
+    
+      return Math.min(...list)
+  }
+  
+  var max = function(list){
+      
+      return Math.max(...list);
+  }
