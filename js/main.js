ffaeebb48894a74b7defb991ed988028c8ca04e9
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-18 23:42:44 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -412,6 +412,18 @@ const reverseSeq = n => {
 
 /* 
 
-27. How much wood would a woodchuck chuck if a woodchuck could chuck wood?
+27. Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
 
 */
+
+function disemvowel(str) {
+  return str.replace( /[aeiouAEIOU]/g, '' );
+}
