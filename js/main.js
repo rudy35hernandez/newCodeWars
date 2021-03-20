ac34c13271b9a91bea018b668d79d212b2a77a9c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-19 23:06:00 -0700

    added question

--- js/main.js
+++ js/main.js
@@ -447,6 +447,10 @@ function disemvowel(str) {
 
 /*
 
-28.
+28. You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
 
 */
