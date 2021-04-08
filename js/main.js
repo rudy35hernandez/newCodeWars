6bad45fbb19723493b811ca80dac463f958e48e1
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-04-08 00:03:09 -0700

    added hard challenge and answer

--- js/main.js
+++ js/main.js
@@ -619,3 +619,23 @@ function points(games) {
 function makeUpperCase(str) {
   return str.toUpperCase()
 }
+
+
+/* 
+
+35. Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
+(The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+
+Example:
+
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+
+If array is empty, null or None, or if only 1 Element exists, return 0.
+Note:In C++ instead null an empty vector is used. In C there is no null. ;-)
+
+*/
+
+function sumArray(array) {
+  return array ? array.sort((a,b) => a-b).slice(1, array.length - 1).reduce((sum, item)=> sum + item,0) : 0
+}
