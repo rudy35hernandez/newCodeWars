68c8bbb2c285816c426453c1200e1a61dcc839ce
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-04-12 23:22:07 -0700

    added question

--- js/main.js
+++ js/main.js
@@ -677,5 +677,5 @@ that your fuel is running out and the nearest pump is 50 miles away! You know th
 runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function
 that tells you if it is possible to get to the pump or not. Function should return true (1 in Prolog) if 
 it is possible and false (0 in Prolog) if not. The input values are always positive.
-
+what is your answer
 */
