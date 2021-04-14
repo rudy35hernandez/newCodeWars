983f4fdb9416a9c486d1e96af21a21fad50d1dc0
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-04-13 23:46:33 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -727,3 +727,19 @@ function getMiddle(s)
   }
   return s.substring(position, position + length)
 }
+
+/*
+
+39. There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may 
+add your point to the given array!
+
+*/
+
