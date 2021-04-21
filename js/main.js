54800a76d3d7241cc01aff395ebd68f30786c8fb
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-04-21 11:23:44 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -1021,3 +1021,16 @@ solution('abc', 'd') // returns false
 function solution(str, ending){
   return str.endsWith(ending)
 }
+
+
+/*
+
+7. Complete the function that accepts a string parameter, and reverses each word in the string. 
+All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+
+*/
+
