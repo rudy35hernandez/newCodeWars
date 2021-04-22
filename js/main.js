b5735c0d8447179067788fe092cc67c3a5f66e21
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-04-22 15:53:18 -0700

    added 2 challenges

--- js/main.js
+++ js/main.js
@@ -836,3 +836,51 @@ function highAndLow(numbers){
   numbers = numbers.split(' ');
   return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
 }
+
+ /****************************************************************************************************
+
+5 String Methods for class!!!!
+
+******************************************************************************************************/
+
+/*
+
+1. Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth 
+(2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, 
+he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, 
+check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+*/
+
+String.prototype.toJadenCase = function () {
+  return this.split(' ').map(word => word[0].toUpperCase() + word.substring(1)).join(' ');
+}
+
+
+
+
+/* 
+
+2. An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function 
+that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. 
+Ignore letter case.
+
+isIsogram("Dermatoglyphics") == true
+isIsogram("aba") == false
+isIsogram("moOse") == false // -- ignore letter case
+
+*/
+
+function isIsogram(str){
+  let allLower = str.toLowerCase();
+  let arr = allLower.split('');
+  let duplicate = arr.filter((letter, index) => arr.indexOf(letter)!== index);
+   return duplicate.length > 0 ? false : true;
+  }
