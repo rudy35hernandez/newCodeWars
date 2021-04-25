e8a0e11f446fe9033a3c1658b9297baa1e9b0a0b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-04-25 00:02:16 -0700

    adding another challenge

--- js/main.js
+++ js/main.js
@@ -928,3 +928,10 @@ DNAStrand ("GTAT") // return "CATA"
 function DNAStrand(dna){
   return dna.replace(/[A]/g, "t").replace(/[T]/g, "a").replace(/[G]/g, "c").replace(/[C]/g, "g").toUpperCase()
 }
+
+
+/*
+
+4.
+
+*/
