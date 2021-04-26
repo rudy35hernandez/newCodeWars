2d3ca6664b7ef2ee64296aeff5005679a0beeb62
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-04-25 23:52:45 -0700

    added answer was tough

--- js/main.js
+++ js/main.js
@@ -968,6 +968,23 @@ function maskify(cc){
 
 /* 
 
-5.
+5. ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but 
+exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+
+Examples
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
 
 */
+
+
+function validatePIN (pin) {
+  if((pin.length == 4 || pin.length == 6) && Number.isInteger(Number(pin))){
+    return true
+  } else{
+    return false
+  }
+}
