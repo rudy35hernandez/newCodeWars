1015812e86fa2bf8314303580a8d4d29a953d57d
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-04-26 23:18:09 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -1004,3 +1004,15 @@ function validatePIN (pin) {
   return false;
 
 }
+
+
+/*
+
+6. Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+*/
