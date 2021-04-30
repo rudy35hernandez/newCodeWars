4643d5239fb283c325a1d8bd04f91f0627dc7171
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-04-29 22:46:17 -0700

    added answer 2nd TRYYYYYYYYYY

--- js/main.js
+++ js/main.js
@@ -1056,3 +1056,12 @@ countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 countBy(2,5) === [2,4,6,8,10]
 
 */
+
+function countBy(x, n) {
+  var z = [];
+  for(let i = 1; i <= n; i++){
+    let answer = x * i
+      z.push(answer)
+  }
+  return z;
+}
