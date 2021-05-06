53808131a0afd8fc011aee33f9219388b107eecd
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-05-06 00:05:45 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -1082,3 +1082,18 @@ function sumTwoSmallestNumbers(numbers) {
   let sorted = numbers.sort((a,b) => a-b);
 return sorted[0] + sorted[1]
 }
+
+/*
+
+3. Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.:
+
+*/
+
