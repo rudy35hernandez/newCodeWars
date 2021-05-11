1fdd049957a419072107c8f99fee1b57a5319029
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-05-10 23:02:37 -0700

    added answer with small help

--- js/main.js
+++ js/main.js
@@ -1193,4 +1193,28 @@ function removeSmallest(numbers) {
 
 /* 
 
-1. 
+1. Story:
+
+Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't 
+give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it 
+for the lowest possible price and sell it at the highest.
+
+Task
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Examples
+minMax([1,2,3,4,5])   == [1,5]
+minMax([2334454,5])   == [5, 2334454]
+minMax([1])           == [1, 1]
+
+*/
+
+function minMax(arr){
+
+  let sorted = arr.sort((a,b) => a-b)
+  
+  let {0 : a ,[sorted.length -1] : b} = sorted
+
+  return [a, b]
+}
+
