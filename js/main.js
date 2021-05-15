9a9436c8bcd6d5312e8fa0e8cfb3f0a07764f8ee
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-05-15 00:04:52 -0700

    finished leons hw

--- js/main.js
+++ js/main.js
@@ -1715,7 +1715,9 @@ function multiplyTogether(arr){
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 2. Two:
-You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed 'age' of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+You will be given an array of all the family members' ages, in any order. The ages will be given in 
+whole numbers, so a baby of 5 months, will have an ascribed 'age' of 0. Return a new array with 
+[youngest age, oldest age, difference between the youngest and oldest age].
 
 //////////////////////answer
 
