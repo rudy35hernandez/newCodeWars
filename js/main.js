812ae8a96774ec034f4a68bb818212437e52e07b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-05-18 00:01:21 -0700

    added solution

--- js/main.js
+++ js/main.js
@@ -1769,3 +1769,9 @@ Examples
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
 
+*/
+
+function sortArray(array) {
+  let odd = array.filter((item) => item % 2 !== 0).sort((a,b) => a - b)
+  return array.map((item) => item % 2 ? odd.shift() : item )
+  }
