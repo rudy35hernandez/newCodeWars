66654bf2e43532dde35d3051a6c3600967553d35
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-05-19 23:53:26 -0700

    added some string methods and their description

--- js/main.js
+++ js/main.js
@@ -1784,4 +1784,55 @@ function sortArray(array) {
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////
   
-  
+  */
+
+  /*
+
+  1. It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is 
+  going to make the most money?
+
+  Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+  If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money 
+  and must receive only his stated salary.
+
+  Return the total figure the individual will receive as a string prefixed with "£" (= "\u00A3", JS, Go, Java and 
+  Julia), "$" (C#, C++, Ruby, Clojure, Elixir, PHP, Python, Haskell and Lua) or "¥" (Rust). 
+  */
+
+  function bonusTime(salary, bonus) {
+// your code here
+function bonusTime(salary, bonus) {
+  // your code here
+  let more = salary * 10;
+    return bonus ? `£${more}` : `£${salary}`
+  } 
+}
+
+/*
+
+2. 
+
+*/
+
+
+/*
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                             String methods, descriptions and examples of them                            //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+
+/*
+
+1. charAt()
+
+Description: charAt is a string method aimed to close in on a particular character in a string. When you put insert
+a number in the parameter, that number is the index of the character in the string.
+
+Ex. 1
+
+let name = 'Rudy'
+
+name.charAt(2) //// returns 'd' because d is at index 2.
+
+Ex. 2
+
