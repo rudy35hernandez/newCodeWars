204cd7a494d600028d6015808f7e2f3286c7625d
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-05-22 21:50:01 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -1862,6 +1862,19 @@ var capitals = function (word) {
 };
 
 
+/*
+
+5. Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck!
+
+*/  
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                             String methods, descriptions and examples of them                            //
