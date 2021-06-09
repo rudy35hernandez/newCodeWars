4fce14e38414559394f315956e1db5874f0797ea
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-06-09 11:47:04 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -2459,6 +2459,6 @@ function descendingOrder(n){
 
 13. Simple, given a string of words, return the length of the shortest word(s).
 
-String will never be empty and you do not need to account for different data types.
+String will never be empty and you do not need to account for different data types. get it.
 
 */
