d0558204d6d96120048d3b5d07b001592f96b2a3
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-06-10 11:46:08 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -2453,3 +2453,12 @@ function descendingOrder(n){
   let sorted = stringIt.split('').sort((a,b)=> b - a).join('');
   return Number(sorted)
 }
+
+
+/*
+
+13. Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+*/
