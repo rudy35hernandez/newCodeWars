a052bb54725c0998c56ba0fe66620d191ee50ed0
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-06-11 18:34:13 -0700

    added another answer

--- js/main.js
+++ js/main.js
@@ -2472,7 +2472,8 @@ function findShort(s){
 
 /*
 
-14. Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+14. Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean 
+and be case insensitive. The string can contain any char.
 
 Examples input/output:
 
@@ -2483,3 +2484,4 @@ XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 XO("zzoo") => false
 
 */
+
