07e0f688e9cd29292ff7e5ac775e09a0a53968ca
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-06-16 12:39:28 -0700

    added solution I almost had

--- js/main.js
+++ js/main.js
@@ -2606,3 +2606,14 @@ function nbYear(p0, percent, aug, p) {
   }
   return finalCatch.length;
 }
+
+/////SHOULD HAVE GOTTEN THIS ANSWER
+
+function nbYear(p0, percent, aug, p) {
+  let count = 0
+  while(p0 < p){
+    p0 += p0*(percent/100)+aug;
+    count++
+  }
+return count
+}
