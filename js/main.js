fa21cfa248dadf241e0b5428e41dff22e9eae043
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-06-15 21:17:13 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -2517,3 +2517,17 @@ let x = str.match(/x/gi)
 let o = str.match(/o/gi)
 return(x && x.length == o && o.length);
 }
+
+
+/*
+
+15. In this kata you will create a function that takes a list of non-negative integers and strings and returns 
+a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+*/
+
