536abf69d74179d41e89555d94dbbc22f607b0f0
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-06-19 13:28:34 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -2679,6 +2679,15 @@ const quarterOf = (month) => {
 
 /*
 
-20.
+20. Write function bmi that calculates body mass index 
+(bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
 
 */
