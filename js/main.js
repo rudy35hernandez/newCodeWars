3b20cfe4fb36d2e2e61e2532aeec6959971d777b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-06-22 23:24:25 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -2794,3 +2794,43 @@ var Ball = function(ballType) {
   }
   
 };
+
+/*
+
+Issue
+Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+The pipes connecting your level's stages together need to be fixed before you receive any more complaints. Each
+pipe should be connecting, since the levels ascend, you can assume every number in the sequence after the first 
+index will be greater than the previous and that there will be no duplicates.
+
+Task
+Given the a list of numbers, return the list so that the values increment by 1 for each index up to the maximum 
+value.
+
+Example
+Input: 1,3,5,6,7,8
+
+Output: 1,2,3,4,5,6,7,8
+
+*/
+
+function pipeFix(numbers){
+  let newArray = []
+  let lowest = Math.min(...numbers)
+  let highest = Math.max(...numbers)
+  
+  for(let i = lowest; i <= highest; i++){
+    newArray.push(i)
+  }
+  return newArray
+}
+
+
+/*
+
+Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+*/
