53a7ee437ab7927e07acdd98235a70dc2b6e096f
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-06-30 22:25:42 -0700

    added solution

--- js/main.js
+++ js/main.js
@@ -3060,3 +3060,15 @@ divisors(4)  = 3  // 1, 2, 4
 divisors(5)  = 2  // 1, 5
 divisors(12) = 6  // 1, 2, 3, 4, 6, 12
 divisors(30) = 8  // 1, 2, 3, 5, 6, 10, 15, 30
+*/
+
+function getDivisorsCnt(n){
+    let divisors = [];
+  for(let i = 1; i <= n; i++){
+    if(n % i === 0){
+     divisors.push(i)
+    }
+   
+  }
+  return divisors.length
+}
