2ad9b80e4798481981e10d9822d11a4f00f7e938
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-05 02:00:42 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -3163,6 +3163,13 @@ const sequenceSum = (begin, end, step) => {
 };
 
 /*
-  prepping next challenge
+  Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array 
+  or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
   
 */
+
