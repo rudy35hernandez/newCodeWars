7aacc3e69a91bc318925b81acce0df9c28ab4167
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-04 21:21:50 -0700

    added another challenge kata 7

--- js/main.js
+++ js/main.js
@@ -3075,4 +3075,9 @@ function getDivisorsCnt(n){
 
 /*
 
+Write a small function that returns the values of an array that are not odd.
+
+All values in the array will be integers. Return the good values in the order they are given.
+
 */
+
