1cb7c195598bff4e486932cfa354ad07bedc125c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-08 14:05:24 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -3180,3 +3180,21 @@ function solution(nums){
     return nums.sort((a,b)=> a-b)
   }
 }
+
+
+/*
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                           July Daily Standup
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+/*
+July 7, 2021
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+*/
+
+
