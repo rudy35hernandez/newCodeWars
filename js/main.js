627127538e26f082098d195ea89c7fde4d398dcd
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-09 23:32:20 -0700

    added solution to our daily challenge

--- js/main.js
+++ js/main.js
@@ -3198,3 +3198,6 @@ For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 */
 
 
+function squareSum(numbers){
+  return numbers.map(num=> num ** 2).reduce((sum, num)=> sum + num)
+}
