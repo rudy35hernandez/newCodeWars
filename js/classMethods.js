3f8e3c68623852b9b69712a10f52be129b56acb2
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-15 17:20:02 -0700

    added challenge

--- js/classMethods.js
+++ js/classMethods.js
@@ -46,3 +46,24 @@ function newString(s1, s2){
   
   console.log(newString('alkjdfkjjs', 'wieokljsdf'))
   console.log(newString('adgk', 'fnmwad'), 'adfgkmnw')
+
+  /// Easier WAY!!!
+
+  const newString2((s1,s2) => [...newSet(s1+s2)].sort().join(''))
+
+
+  
+// John has invited some friends. His list is:
+
+// s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+// Could you make a program that
+
+// makes this string uppercase
+// gives it sorted in alphabetical order by last name.
+// When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+// So the result of function meeting(s) will be:
+
+// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+
+function 
