f0030cfbc1159155a6408a66b31d86b70d2327c5
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-16 23:13:54 -0700

    added daily standup

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -161,4 +161,9 @@ function stringTransformer(str) {
 
 /*
 
+Count all the occurring characters in a string. If you have a string like aba, then the result should be 
+{'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+
 */
