51252de03b506a40b7a0be8b0cefb28fe00f5997
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-20 01:00:44 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -183,3 +183,21 @@ function count (string) {
   return wholeCount
 }
 
+
+/*
+
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+
+Test.assertEquals(high('man i need a taxi up to ubud'), 'taxi');
+Test.assertEquals(high('what time are we climbing up the volcano'), 'volcano'); 
+Test.assertEquals(high('take me to semynak'), 'semynak'); 
+
+*/
