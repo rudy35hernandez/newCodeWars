e901be89df95232daec92732723514d5ad83f872
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-21 22:58:35 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -250,6 +250,21 @@ function duplicateEncode(word){ /// solved myself!!!!!!!!!!!!!!!!!
 
 /*
 
+July 21, 2021
 
+In this Kata, you will be given an array of unique elements, and your task is to rearrange the values so that the 
+first max value is followed by the first minimum, followed by second max value then second min value, etc.
+
+For example:
+
+solve([15,11,10,7,12]) = [15,7,12,10,11]
+The first max is 15 and the first min is 7. The second max is 12 and the second min is 10 and so on.
+
+Test.assertDeepEquals(solve([15,11,10,7,12]),[15,7,12,10,11]);
+Test.assertDeepEquals(solve([91,75,86,14,82]),[91,14,86,75,82]);
+Test.assertDeepEquals(solve([84,79,76,61,78]),[84,61,79,76,78]);
+Test.assertDeepEquals(solve([52,77,72,44,74,76,40]),[77,40,76,44,74,52,72]);
+Test.assertDeepEquals(solve([1,6,9,4,3,7,8,2]),[9,1,8,2,7,3,6,4]);
+Test.assertDeepEquals(solve([78,79,52,87,16,74,31,63,80]),[87,16,80,31,79,52,78,63,74]);
 
 */
