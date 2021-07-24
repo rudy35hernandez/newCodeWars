608d42c2094d019a3e4b9cd154f43a07b2dd5330
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-24 01:06:41 -0700

    added new daily challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -298,3 +298,21 @@ function switcher(x){ /// got this solution on my own
   let alphabet = ' zyxwvutsrqponmlkjihgfedcba!? '
   return x.map((num, index, arr)=> alphabet[Number(num)]).join('')
 }
+
+/*
+
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character 
+in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the 
+given string.
+
+All inputs will be valid.
+
+Test.assertSimilar(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+Test.assertSimilar(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 
+'volcano', 'up', 'what']); 
+Test.assertSimilar(last('take me to semynak'), ['take', 'me', 'semynak', 'to']); 
+
+*/
+
