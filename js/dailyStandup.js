6527c590d9634ee170e8e522e12a9962e0b1d8a0
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-24 01:05:28 -0700

    added challenge and solution

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -281,3 +281,20 @@ Yet to solve this!!!
 /*
 
 July 22, 2021
+
+Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of 
+alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by 
+'27', '28' and '29' respectively.
+
+All inputs will be valid.
+
+Test.assertEquals(switcher(['24', '12', '23', '22', '4', '26', '9', '8']), 'codewars');
+Test.assertEquals(switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4']), 'btswmdsbd kkw'); 
+Test.assertEquals(switcher(['4', '24']), 'wc'); 
+
+*/
+
+function switcher(x){ /// got this solution on my own
+  let alphabet = ' zyxwvutsrqponmlkjihgfedcba!? '
+  return x.map((num, index, arr)=> alphabet[Number(num)]).join('')
+}
