c59221d20400051fb809a40cb5ec66d58d8d3a0a
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-25 20:59:33 -0700

    added challenge from codewars

--- js/main.js
+++ js/main.js
@@ -2871,3 +2871,17 @@ Grab the the indeces of both numbers that equal the target number.
 function getTarget(arr, target){
   
 }
+
+
+/*
+
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of 
+each of the number's decimal digits. For example:
+
+  sumDigits(10);  // Returns 1
+  sumDigits(99);  // Returns 18
+  sumDigits(-32); // Returns 5
+Let's assume that all numbers in the input will be integer values.
+
+*/
+
