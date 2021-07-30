5cd6445f95a946f11af94cf4adbc336d4d6bb3d6
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-07-30 00:04:06 -0700

    added challenge for july 30

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -444,3 +444,27 @@ var capitals = function (word) { /// forgot about the Number.isInteger method. H
     .map((letter,index) => letter == letter.toUpperCase() ? index : letter)
     .filter(el => Number.isInteger(el))
 }      
+
+
+/*
+
+July 30, 2021
+
+You will be given an array of strings and your task is to remove all consecutive duplicate letters from each 
+string in the array.
+
+For example:
+
+dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+dup(["kelless","keenness"]) = ["keles","kenes"].
+Strings will be lowercase only, no spaces 
+
+Test.assertDeepEquals(dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars',
+'picaniny','hubububo']);
+Test.assertDeepEquals(dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese']);
+Test.assertDeepEquals(dup(["kelless","keenness"]), ['keles','kenes']);
+Test.assertDeepEquals(dup(["Woolloomooloo","flooddoorroommoonlighters","chuchchi"]), ['Wolomolo',
+'flodoromonlighters','chuchchi']);
+
+*/
+
