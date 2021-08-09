91ca406781ca409b05369a8badfc429a5eab48f9
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-08-08 23:26:58 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -3074,3 +3074,26 @@ for(let i = 1; i <= n; i++){
 }
   return empty
 }
+
+
+/*
+
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined 
+as having health <= 0.
+
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the 
+Fighter objects.
+
+Example:
+function Fighter(name, health, damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+        this.toString = function() { return this.name; }
+}
+
+
+*/
+
