9dc7c606839425c25a0a55f1061da1171e4b1398
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-08-09 00:25:37 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -660,3 +660,14 @@ function fizzBuzz(n){
   }
   return output
 }
+
+
+/* 
+
+Aug 10, 2021
+
+Given a string of characters as input, write a function that returns it with the characters reversed. No Reverse 
+Method (well brute force it first, but then no reverse method)!
+
+*/
+
