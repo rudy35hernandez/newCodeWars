88fac915c74bdc75ba6b5aef8208316b7d4e24c5
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-08-14 00:34:38 -0700

    added another solution from class

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -769,6 +769,17 @@ function reverseArray(arr){
   return newArr.slice(1)
 }
 
+// the class solution
+
+function reverseArr(arr){
+  for(let i = 0; i < arr.length / 2; i++){
+    const temp = arr[i]
+    arr[i] = arr[arr.length - 1 - i]
+    arr[arr.length - 1 - i] = temp
+  }
+  return arr
+}
+
 
 /*
 
