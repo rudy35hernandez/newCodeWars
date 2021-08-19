86daa43e5c32608cfbba0d894e68a31c64d41e12
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-08-18 21:48:14 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -875,3 +875,15 @@ function ransomNote(str, note){
   })
   return possible
 }
+
+/*
+
+Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
+
+describe("Two Sum", () => {
+ it("Should implement two sum", () => {
+  assert.deepEqual(twoSum([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]]);
+ });
+});
+
+*/
