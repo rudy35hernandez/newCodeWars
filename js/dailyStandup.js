4a101dbf04e41893d81e90dc9889b8438739bb52
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-08-24 10:17:03 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -1053,3 +1053,27 @@ function goodPairs(arr){
   }
   return count
 }
+
+
+/* 
+
+Aug 24, 2021
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if 
+every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+*/
+
