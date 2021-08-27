85cd4bd38acf0447c0b9221bda3ffbd80ae32f9b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-08-27 08:38:34 -0700

    added edits

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -1160,7 +1160,8 @@ function checkStones(str1, str2){
 
 Aug 27, 2021
 
-The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number
+is the sum of the two preceding ones, starting from 0 and 1. That is,
 
 F(0) = 0, F(1) = 1
 F(n) = F(n - 1) + F(n - 2), for n > 1.
