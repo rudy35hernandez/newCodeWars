0e55bfbdb51d50c28fda3ed42917fa07be6b515a
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-08-28 23:03:50 -0700

    added solution

--- js/main.js
+++ js/main.js
@@ -3260,3 +3260,15 @@ String has no spaces
 Size is always positive
 
 */
+
+function splitInParts(s, partLength){
+  let newStr = ''
+  for(let i = 0; i < s.length; i++){
+    if(i % partLength === 0){
+      newStr += ` `+s[i]
+    } else {
+      newStr += s[i]
+    }
+  }
+  return newStr.slice(1)
+}
