3361489224d075fc44d57d54d53dcc796a9a5870
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-02 14:43:56 -0700

    added recursion challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -1269,3 +1269,20 @@ function factorialRecursion(num){
 120 * factorialRecursion(2-1) = 120 
 
 */
+
+
+/*
+
+Sep 2, 2021
+
+Factorial
+
+Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+Test.assertEquals(mygcd(30,12),6)
+Test.assertEquals(mygcd(8,9),1)
+Test.assertEquals(mygcd(1,1),1)
+
+*/
