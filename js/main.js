4d3f60ebb9cde9ca33cc090157d8d2064496a1e5
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-05 23:12:03 -0700

    error in last commit

--- js/main.js
+++ js/main.js
@@ -3389,4 +3389,16 @@ function minValue(values){
 
 
 /*
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
 
+Example:
+
+Input:
+
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+
+'alpha beta gamma delta'
+
+*/
