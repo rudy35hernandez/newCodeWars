f086c334e659bbc0513ed2592578fbc6f29ffd16
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-06 22:40:10 -0700

    got some help online, need to remember this solution

--- js/main.js
+++ js/main.js
@@ -3462,3 +3462,7 @@ Input may be any positive or negative integer (including 0).
 You can assume that all inputs are valid integers.
 
 */
+
+function roundToNext5(n){
+  return Math.ceil(n/5) * 5
+}
