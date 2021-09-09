ff9d0421168d411da90aed6e9ceb79e9199082aa
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-09 08:15:58 -0700

    added soltution for challenge

--- js/main.js
+++ js/main.js
@@ -3523,3 +3523,7 @@ The solution would work like the following:
 getEvenNumbers([2,4,5,6]) // should == [2,4,6]
 
 */
+
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(num => num % 2 == 0 )
+}
