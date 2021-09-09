8e7984305e93f8be4dc60087da614d73f5e0fc18
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-08 23:08:45 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -3490,9 +3490,21 @@ Simple remove duplicates
 */
 
 function solve(s){
-  let lowAlph = 'abcdefghijklmnopqrstuvwxyz'
+  let lowAlph = 'abcdefghijklmnopqrstuvwxyz'  /// create a lowercase library
   
   let totalLow = s.split('').filter((letter,index,arr)=> lowAlph.includes(letter)).join('').length
   
   return totalLow >= s.length / 2 ? s.toLowerCase() : s.toUpperCase()
 }
+
+
+/*
+
+Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever 
+solution.
+
+The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that 
+is also greater or equal to 1.
+
+*/
+
