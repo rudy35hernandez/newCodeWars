3b7a68fc320ea9b7cdf0b1a3bf555a2004724e1b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-11 22:32:49 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -3544,7 +3544,21 @@ Example:
 
 
 var greet = function(name) {
-  let lowName = name.toLowerCase().slice(1)  //// returned all letters in name in lowercase after the first char
-  let first = name[0].toUpperCase()
+  let lowName = name.toLowerCase().slice(1)  //// made name in lowercase, sliced beginning at index 1
+  let first = name[0].toUpperCase() /// 
   return `Hello ${first}${lowName}!`
 };
+
+/*
+
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the 
+integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return 
+the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+Example:
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime"
+
+*/
+
