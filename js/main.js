e170662e04e872a35c3b4d7159c1814a60a087ce
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-17 23:13:23 -0700

    added challenge and solution

--- js/main.js
+++ js/main.js
@@ -3666,3 +3666,25 @@ With floats it can happen that results depends on the operations order. To calcu
 (3600 * delta_distance) / s.
 
 */
+
+
+/*
+
+Challenge:
+
+Given a two-dimensional array of integers, return the flattened version of the array with all the integers in 
+the sorted (ascending) order.
+
+Example:
+
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+Addendum:
+
+Please, keep in mind, that JavaScript is by default sorting objects alphabetically.
+
+*/
+
+function flattenAndSort(array) {
+  return [].concat(...array).sort((a,b) => a-b)
+}
