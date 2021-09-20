04a4fed646a82db6d6f7d3ea82ec08ab0ce4b2ae
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-19 21:43:34 -0700

    added challenge, similar to the one the other day

--- js/main.js
+++ js/main.js
@@ -3716,3 +3716,16 @@ function sumTriangularNumbers(n) {
   }
 return count
 }
+
+
+
+/*
+
+Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level 
+of flattening.
+
+flatten([1,2,3]) // => [1,2,3]
+flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+flatten([[[1,2,3]]]) // => [[1,2,3]]
+
+*/
