c51240ff0762b1c02f57749687af359023ab422f
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-21 21:15:34 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -3688,3 +3688,23 @@ Please, keep in mind, that JavaScript is by default sorting objects alphabetical
 function flattenAndSort(array) {
   return [].concat(...array).sort((a,b) => a-b)
 }
+
+
+/*
+
+Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+
+Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.) obtained by continued summation of 
+the natural numbers 1, 2, 3, 4, 5, etc."
+
+[01]
+02 [03]
+04 05 [06]
+07 08 09 [10]
+11 12 13 14 [15]
+16 17 18 19 20 [21]
+e.g. If 4 is given: 1 + 3 + 6 + 10 = 20.
+
+Triangular Numbers cannot be negative so return 0 if a negative number is given.
+
+*/
