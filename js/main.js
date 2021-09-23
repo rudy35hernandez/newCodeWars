ab95c4679e0c858726ca21a3ba4ca28eac16e431
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-22 22:32:29 -0700

    added challenge

--- js/main.js
+++ js/main.js
@@ -3771,3 +3771,12 @@ function rowWeights(array){ //// very brute force.
   return [evens, odds] 
   
 }
+
+
+/*
+
+Find the number with the most digits.
+
+If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+*/
