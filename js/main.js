84bc2e49ca14f7423f2074b55b82bf6c04de5c6b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-25 22:17:28 -0700

    added solution, lots of help, write some notes to walk through the prob to understand it

--- js/main.js
+++ js/main.js
@@ -3842,3 +3842,16 @@ Examples
  7  -->  [0,  1,  3,  6,  10,  15,  21,  28]
 
  */
+
+ function sumOfN(n) {
+  var array = [];
+  array[0] = 0;
+  for (var i = 1; i < Math.abs(n)+1; i++) {
+    if (n > 0) {
+      array[i] = array[i-1]+i;
+    } else {
+      array[i] = array[i-1]-i;
+    }
+  }
+  return array;
+};
