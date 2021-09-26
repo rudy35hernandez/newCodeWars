2123fae09ddc5062e9ff8ddfdf7cb52f4656ed4a
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-09-25 22:11:16 -0700

    added tough challenge that may need recursion

--- js/main.js
+++ js/main.js
@@ -3826,3 +3826,19 @@ function multiplyAll(arr){
   }
   return total
 }
+
+
+/*
+
+[ 0,  1,    3,      6,   ...]
+  0  0+1  0+1+2  0+1+2+3
+Your Task
+Complete the function that takes an integer n and returns a list/array of length abs(n) + 1 of the arithmetic 
+series explained above. Whenn < 0 return the sequence with negative terms.
+
+Examples
+ 5  -->  [0,  1,  3,  6,  10,  15]
+-5  -->  [0, -1, -3, -6, -10, -15]
+ 7  -->  [0,  1,  3,  6,  10,  15,  21,  28]
+
+ */
