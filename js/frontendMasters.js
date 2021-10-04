db7b62f37be1616355225a7566a4c10ce5954f8b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-10-04 09:12:21 -0700

    added challenge

--- js/frontendMasters.js
+++ js/frontendMasters.js
@@ -347,3 +347,82 @@ _.map = function(list, callback){
   })
   return storage
 }
+
+
+/////////////////////// filter() practice next ///////////////////////////////////
+
+const videoData = {
+  {
+    name: "Miss Scarlet",
+    present: true,
+    rooms: [
+      {kitchen: false},
+      {ballroom: false},
+      {conservatory: false},
+      {'dining room': false},
+      {'billiard room': false},
+      {library: false},
+    ]
+  },
+  {
+    name: "Mrs. White",
+    present: false,
+    rooms: [
+      {kitchen: false},
+      {ballroom: false},
+      {conservatory: false},
+      {'dining room': false},
+      {'billiard room': false},
+      {library: false},
+    ]
+  },
+  {
+    name: "Reverend King",
+    present: false,
+    rooms: [
+      {kitchen: false},
+      {ballroom: false},
+      {conservatory: false},
+      {'dining room': false},
+      {'billiard room': false},
+      {library: false},
+    ]
+  },
+  {
+    name: "Rusty",
+    present: false;
+    rooms: [
+      {kitchen: false},
+      {ballroom: false},
+      {conservatory: false},
+      {'dining room': false},
+      {'billiard room': false},
+      {library: false},
+    ]
+  },
+  {
+    name: "Colonel Mustard",
+    present: true,
+    rooms: [
+      {kitchen: false},
+      {ballroom: false},
+      {conservatory: false},
+      {'dining room': false},
+      {'billiard room': false},
+      {library: false},
+    ]
+  },
+  {
+    name: "Professor Plum",
+    present: true,
+    rooms: [
+      {kitchen: false},
+      {ballroom: false},
+      {conservatory: false},
+      {'dining room': false},
+      {'billiard room': false},
+      {library: false},
+    ]
+  }
+
+}
