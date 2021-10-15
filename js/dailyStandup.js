2660d52548a078c4925b04e1649a573e00ae3f52
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-10-15 08:39:50 -0700

    added challenge for monday

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -1552,3 +1552,24 @@ function evenNumbers(arr, num){
 function evenNumber2(arr,num){
   return arr.filter(num => num % 2 == 0).slice(-number)
 }
+
+
+/*
+
+Oct 18, 2021
+
+Return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are 
+[1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra 
+outcomes are just duplicates.
+
+solve([[1,2],[4],[5,6]]),4)
+solve([[1,2],[4,4],[5,6,6]]),4)
+solve([[1,2],[3,4],[5,6]]),8)
+solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72)
+
+
+*/
