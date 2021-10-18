3ce7b0c542ea196fff2883d7f6c750a3399ab169
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-10-18 11:51:31 -0700

    added notes

--- js/coltSteel.js
+++ js/coltSteel.js
@@ -4,3 +4,9 @@
 /// machines. Even the same machine will record different times.
 /// For fast algo's, speed measurements may not be precise enough
 
+
+                                //// BIG O SHORTHANDS ///
+
+// 1. Arithmic operations are constant
+// 2. Assignments are constant
+// 3. Accessing elements in an Array (by index) or object (by key) is constant. Ex. arr[3]
