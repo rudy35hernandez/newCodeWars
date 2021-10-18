27c63eddf66906f210c28d4c586a893e99e2c3fc
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-10-18 10:28:27 -0700

    added note

--- js/coltSteel.js
+++ js/coltSteel.js
@@ -1,2 +1,6 @@
+                    // TIME COMPLEXITY //
+
 /// Problem with time complexity, not reliable because you will most likely get different times with different
-/// machines.
+/// machines. Even the same machine will record different times.
+/// For fast algo's, speed measurements may not be precise enough
+
