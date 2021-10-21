cfd5e5cadae5a07b4bcd29700814eab6b8a577c7
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-10-20 21:13:18 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -1625,3 +1625,19 @@ function nthSmallestNum(arr, num){
 function nthSmallestNum2(arr,num){
   return arr.sort((a,b)=> a-b)[num-1]
 }
+
+/*
+
+Oct 20, 2021
+
+The goal of this exercise is to convert a string to a new string where each character in the new string is 
+"(" if that character appears only once in the original string, or ")" if that character appears more than once 
+in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+
+*/
