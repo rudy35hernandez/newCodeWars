9dfd409ce19d39d3bf217dc47e35771216cdb0ad
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-10-23 10:47:02 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -1691,4 +1691,17 @@ function duplicateEncode3(word){
 
 /*
 
-Oct
+Oct 24, 2021
+
+Given a string, return a new string that has transformed based on the input:
+
+Change case of every character, ie. lower case to upper case, upper case to lower case.
+Reverse the order of words from the input.
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+For example:
+
+"Example Input" ==> "iNPUT eXAMPLE"
+You may assume the input only contain English alphabet and spaces.
+
+*/
