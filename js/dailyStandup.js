2336b6fff18d39340f5c801c806ac1721d446523
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-10-25 22:58:19 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -1723,3 +1723,21 @@ function reverseCase(str){
 
 console.log(reverseCase("sTRING Example"), "eXAMPLE String")
 console.log(reverseCase("Yes mAN"), "Man yES")
+
+/*
+
+Oct 26, 2021
+
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final 
+character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in 
+the given string.
+
+All inputs will be valid.
+
+last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']
+last('take me to semynak'), ['take', 'me', 'semynak', 'to']
+
+*/
