062245bd0104ce479033eaa2c8bb38ee36c998f7
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-10-30 08:45:57 -0700

    added challenge for the day

--- js/coltSteel.js
+++ js/coltSteel.js
@@ -22,3 +22,6 @@
 // Most primitives (booleans, numbers, undefined, null) are constant space
 // Strings require O(n) space (where n is the string length)
 // Reference types are generally O(n), where n is the length (for arrays), or the number of keys (for objects)
+
+/////////////////////// Logarithms ////////////////////////
+
--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -1829,3 +1829,25 @@ function subtractArr(a, b){
 console.log(subtractArr([1,2], [1]), [2]);
 console.log(subtractArr([2,2,1,4], [2]), [1, 4]);
 console.log(subtractArr([5,5,1,1,2], [5,1]), [2]);
+
+
+/*
+
+Oct 30, 2021
+
+You need to create a function that when provided with a triplet, returns the index of the numerical element 
+that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers.
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+Another example (just to make sure it is clear):
+
+gimme([5, 10, 14]) => 1
+10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+*/
