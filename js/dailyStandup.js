2d4e0d061b8c848ea513a385c83a2944776b353b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-03 22:13:54 -0700

    added challenge for the day

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -1898,3 +1898,21 @@ function sortTheOdds(arr){
 console.log(sortTheOdds([4, 2, 3, 1, 6, 9]), [4, 2, 1, 3, 6, 9])
 console.log(sortTheOdds([7, 1]), [1, 7])
 console.log(sortTheOdds([5, 1, 3, 6, 9, 2, 7]), [1, 3, 5, 6, 7, 2, 9])
+
+
+/*
+
+Nov 3, 2021
+
+Given an array of strings of the same letter type. Return a new array, which will differ in that the length of 
+each element is equal to the average length of the elements of the previous array.
+
+A few examples:
+
+['u', 'y'] =>  ['u', 'y'] // average length is 1
+['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+If the average length is not an integer, use Math.round().
+The input array's length > 1
+
+*/
