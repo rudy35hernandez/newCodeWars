5b1084b026e79bb6a1d09a4ee16073df7ea8ce92
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-06 10:35:21 -0700

    oldie but goodie challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2087,3 +2087,15 @@ function shortenStr(str,num){
     return str.slice(0,num) + "..."
   }
 }
+
+
+/*
+
+nov 15, 2021
+
+Given a number as an input, print out every integer from 1 to that number. However, when 
+the integer is divisible by 2, print out “Fizz”; when it’s divisible by 3, print out “Buzz”; when it’s 
+divisible by both 2 and 3, print out “Fizz Buzz”.
+
+*/
+
