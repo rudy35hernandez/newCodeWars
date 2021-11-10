01aef7bf069fee4afba61a1bbe893550ed9dd61b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-10 09:56:07 -0800

    added progress

--- js/main.js
+++ js/main.js
@@ -3955,7 +3955,8 @@ function findLongestWordLength(str) {
 
 Return the factorial of the provided integer.
 
-If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than 
+or equal to n.
 
 Factorials are often represented with the shorthand notation n!
 
@@ -3963,3 +3964,14 @@ For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
 
 Only integers greater than or equal to zero will be supplied to the function. */
 
+
+function factorialize(num) {
+  /// set up new array to add all numbers
+  let newArr = []
+  for(let i = 0; i <= num; i++){
+
+  }
+}
+
+
+console.logo(factorialize(5));
