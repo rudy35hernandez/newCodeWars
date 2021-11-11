ea0f75e7272386813498780b48d220abba0206b8
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-11 12:42:50 -0800

    added challenge

--- js/main.js
+++ js/main.js
@@ -3980,3 +3980,14 @@ function factorialize(num) {
 
 
 console.logo(factorialize(5));
+
+
+/*
+
+Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array 
+will contain exactly 4 sub-arrays.
+
+Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+*/
+
