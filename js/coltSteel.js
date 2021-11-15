fe6f446c5c0e65eaacf9fb6cdce53042e5a27122
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-15 10:22:05 -0800

    added challenge, use the frequency counter to solve

--- js/coltSteel.js
+++ js/coltSteel.js
@@ -113,3 +113,11 @@ function hashMap(str){
       }
       return true
   }
+
+
+  //////// ANAGRAM CHALLENGE ////////////
+  
+  /* Given two strings, write a function to determine if the second string is an anagram of the first. An anagram
+  is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+  
