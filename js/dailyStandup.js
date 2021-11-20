2bbff0ffacbc9577fed4c66f8448312f13cddfcf
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-19 21:56:47 -0800

    added solution

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2284,7 +2284,17 @@ checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
 /// greater, it will return the score
 
 function checkExam(array1, array2) {
-
+  let score = 0;
+  for(let i = 0; i < array2.length; i++){
+    if(array2[i] === ""){
+      score += 0;
+    } else if(array2[i] === array1[i]){
+      score += 4;
+    } else {
+      score += -1
+    }
+  }
+  return score < 0 ? 0 : score
  }
 
 
