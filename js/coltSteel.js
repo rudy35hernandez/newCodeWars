3ccaf8eecc48b9f99f4407fa8ed879d578cb07fd
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-19 21:39:53 -0800

    added challenge

--- js/coltSteel.js
+++ js/coltSteel.js
@@ -220,3 +220,4 @@ middle. This time both our pointers will start from the left side
 DETAILS: Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values
 in the array. There can be negative numbers in the array, but it will always be sorted.
 */
+
--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2250,3 +2250,26 @@ function numbers(arr){
 
 console.log(number([[10,0],[3,5],[5,8]]),5);
 console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]),17)
+
+
+/*
+
+Nov 19, 2021
+
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one 
+contains a student's submitted answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for 
+each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string 
+(in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+*/
