3c63cb2768c6156d65f71b868803dd0ea6dc9659
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-23 22:11:59 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2375,3 +2375,21 @@ console.log(factorial(7), 5040)
 function factorial2(n){
   return n ? n * factorial2(n - 1): 1
 }
+
+
+/*
+To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an 
+argument. This function must return another function, which takes a single integer as an argument and returns a new 
+array.
+
+The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+Example:
+
+multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+You must not mutate the original array.
+
+Here's a nice Youtube video about currying, which might help you if this is new to you.
+
+*/
+
