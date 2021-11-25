6c9bc7e4537b90633d693dca31784077d4ebdc54
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-25 10:12:14 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2507,3 +2507,25 @@ console.log(seven(1021), [10, 2])
 console.log(seven(1603), [7, 2])
 console.log(seven(371), [35, 1])
 console.log(seven(483), [42, 1])
+
+
+/*
+
+Nov 26, 2021
+
+Given a 2D array of size m * n. Your task is to find the sum of minimum value in each row.
+
+For Example:
+
+[
+  [1, 2, 3, 4, 5],       // minimum value of row is 1
+  [5, 6, 7, 8, 9],       // minimum value of row is 5
+  [20, 21, 34, 56, 100]  // minimum value of row is 20
+]
+So, the function should return 26 because sum of minimums is as 1 + 5 + 20 = 26
+
+Note: You will be always given non-empty array containing Positive values.
+
+ENJOY CODING :)
+
+*/
