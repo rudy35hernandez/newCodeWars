d248b9b64beaa16a4e23b652229a70a186ccb9b9
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-24 22:47:16 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2401,6 +2401,44 @@ let multiplyAll =
     multiplyBy => 
       arr.map(num => num * multiplyBy)
 
-console.log(multiplyAll([4, 2, 1])(3), [12, 6, 3])
-console.log(multiplyAll([2, 7, 4])(7), [14, 49, 28])
-console.log(multiplyAll([6, 9, 10])(2), [12, 18, 20])
+// console.log(multiplyAll([4, 2, 1])(3), [12, 6, 3])
+// console.log(multiplyAll([2, 7, 4])(7), [14, 49, 28])
+// console.log(multiplyAll([6, 9, 10])(2), [12, 18, 20])
+
+
+/* 
+
+Nov 24, 2021
+
+Introduction
+There is a war and nobody knows - the alphabet war!
+There are two groups of hostile letters. The tension between left side letters and right side letters was too high 
+and the war began.
+
+Task
+Write a function that accepts fight string consists of only small letters and return who wins the fight. When the 
+left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's 
+fight again!.
+
+The left side letters and their power:
+
+ w - 4
+ p - 3
+ b - 2
+ s - 1
+The right side letters and their power:
+
+ m - 4
+ q - 3
+ d - 2
+ z - 1
+The other letters don't have power and are only victims.
+
+Example
+alphabetWar("z");        //=> Right side wins!
+alphabetWar("zdqmwpbs"); //=> Let's fight again!
+alphabetWar("zzzzs");    //=> Right side wins!
+alphabetWar("wwwwwwz");  //=> Left side wins!
+
+*/
+
