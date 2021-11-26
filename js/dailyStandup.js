71c7b8c116a55deda019a011f9fb19b79202d36b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-26 10:14:27 -0800

    added test cases

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2529,3 +2529,5 @@ Note: You will be always given non-empty array containing Positive values.
 
 */
 
+console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]), 9);
+console.log(sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]]), 76)
