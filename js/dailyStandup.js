e3a9a6dff34ab4d890d3556f26c610387d5a1309
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-11-29 23:16:17 -0800

    added function

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2563,5 +2563,12 @@ Note: You will be always given non-empty array containing Positive values.
 
 */
 
+function sumOfMinimums(arr)
+
 console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]), 9);
 console.log(sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]]), 76)
+
+
+
+/*
+
