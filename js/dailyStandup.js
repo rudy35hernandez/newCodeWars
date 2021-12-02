740d5677cd8d0863b72955d4ca6c35663e9de9c8
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-01 22:24:16 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2691,3 +2691,20 @@ function appearsTwice2(arr){
   let unique = [...new Set(arr)];
   return unique.length !== arr.length
 }
+
+
+/*
+
+Dec 1, 2021
+
+If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice stronger than love :-)
+
+The input will always be in lowercase and never be empty.
+
+*/
