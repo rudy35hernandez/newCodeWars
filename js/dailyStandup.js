4823cc07458a40e9ff95cc2672740b051cdc1a52
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-02 22:35:22 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2719,3 +2719,19 @@ function wordsToMarks(string){
   let alphabet = '0abcdefghijklmnopqrstuvwxyz';
   return string.split('').map((letter,index,arr)=> alphabet.indexOf(letter)).reduce((sum, num) => sum + num,0)
 }
+
+/*
+
+Dec 2, 2021
+
+Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+
+Assume that the input n will always be a positive integer.
+
+Examples: (Input --> output)
+
+2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
+
+*/
+
