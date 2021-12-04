a7ff3772f1f7125a65fb026d5060e7f66d0c1832
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-03 23:13:22 -0800

    added solution

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2776,3 +2776,11 @@ The maximum product obtained from multiplying -2 * 7 = -14, and they're adjacent
 /// Next, i want to loop through the array, multiply each index by the next, then push the sum to the empty arr
 /// Now that I have my list of sums, i can just return the maximum num with Math.max(...arr)
 
+function adjacentElementsProduct(array) {
+  let arr = []
+  
+  for(let i = 0; i < array.length-1; i++){
+    arr.push(array[i]*array[i+1])
+  }
+  return Math.max(...arr)
+}
