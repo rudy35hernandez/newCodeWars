caa0fa6685a0cd319e7cca4a4fdaed246f8eef20
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-05 11:10:44 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -2784,3 +2784,24 @@ function adjacentElementsProduct(array) {
   }
   return Math.max(...arr) /// or arr.sort((a,b)=> b-a)[0]
 }
+
+
+/* 
+
+Dec 4, 2021
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element 
+always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+*/
