8914630d241806ffb157fb1a3791028d612b49cc
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-09 11:59:22 -0800

    added some notes

--- js/react.js
+++ js/react.js
@@ -19,3 +19,29 @@ class Person extends Human {
         console.log(this.name)
     }
 }
+
+
+/// Spread operator and rest
+
+const numbers = [1, 2, 3];
+const newNumbers = [...numbers, 4];
+
+/// we can do it with objects also
+
+const person = {
+    name: "Rudy"
+}
+
+const newPerson = {
+    ...person,
+    age: 28,
+}
+
+/// Rest operator
+
+const filter = (...args) => {
+    return args.filter(nums => nums < 4)
+}
+
+console.log(filter(1, 3, 6, 3, 5, 4)) /// returns [1, 2, 3]
+
