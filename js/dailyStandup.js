58d783c6db22eb5f92643e9b1043aecd24131e60
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-28 15:38:51 -0800

    added solution

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -3187,10 +3187,17 @@ See "Sample Tests".
 
 function vertMirror(strng) {
   // Your code
+  return str.split('\n')
+  .map(line => line.split('').reverse().join(''))
+  .join('\n')
 }
 function horMirror(strng) {
   // Your code
+  return str.split('\n')
+  .reverse()
+  .join('\n')
 }
 function oper(fct, s) {
   // Your code
+  return fct(s)
 }
