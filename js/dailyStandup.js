efe7f55fc8fa8a577a8061b3150a6f2d4d328740
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-28 15:32:25 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -3138,3 +3138,47 @@ function numPlusIndex(arr){
 console.log(numPlusIndex([3,2,1,5]), [4,4,4,9])
 console.log(numPlusIndex([9, 2, 6, 8, 3]), [0, 4, 9, 2, 8])
 console.log(numPlusIndex([5, 9, 2, 8]), [6, 1, 5, 2])
+
+
+/*
+
+Dec 28th, 2021
+
+This kata is the first of a sequence of four about "Squared Strings".
+
+You are given a string of n lines, each substring being n characters long: For example:
+
+s = "abcd\nefgh\nijkl\nmnop"
+
+We will study some transformations of this square of strings.
+
+Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+or printed:
+
+vertical mirror   |horizontal mirror   
+abcd --> dcba     |abcd --> mnop 
+efgh     hgfe     |efgh     ijkl 
+ijkl     lkji     |ijkl     efgh 
+mnop     ponm     |mnop     abcd 
+Task:
+Write these two functions
+and
+
+high-order function oper(fct, s) where
+
+fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+
+Examples:
+s = "abcd\nefgh\nijkl\nmnop"
+oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
+Note:
+The form of the parameter fct in oper changes according to the language. You can see each form according to 
+the language in "Sample Tests".
+
+Bash Note:
+The input strings are separated by , instead of \n. The output strings should be separated by \r instead of \n.
+See "Sample Tests".
