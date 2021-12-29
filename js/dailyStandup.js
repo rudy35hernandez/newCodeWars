ede739d1c8e8f335e99fae24c01a7004537eb27e
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-12-29 15:50:00 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -3201,3 +3201,19 @@ function oper(fct, s) {
   // Your code
   return fct(s)
 }
+
+/*
+
+Dec 30, 2021
+
+I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a 
+whole number between 1 and 50.
+
+Example
+n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++
+
+*/
