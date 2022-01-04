c3424471b3d6ee9d4a0b949e1e3a2aa575123858
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-01-03 22:36:58 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -3311,3 +3311,25 @@ function maxNumber(n){
 console.log(maxNumber(1322), 3221)
 console.log(maxNumber(783129), 987321)
 console.log(maxNumber(32164), 64321)
+
+
+/*
+
+Jan 3, 2021
+
+We want to know the index of the vowels in a given word, for example, there are two vowels in the word super 
+(the second and fourth letters).
+
+So given a string "super", we should return a list of [2, 4].
+
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+NOTES
+Vowels in this context refers to: a e i o u y (including upper case)
+This is indexed from [1..n] (not zero indexed!)
+
+*/
+
