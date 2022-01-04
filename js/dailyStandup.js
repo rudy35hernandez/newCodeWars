d91eb430f00a7271ce290f5a98fa662d7a40472c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-01-03 22:48:22 -0800

    added function declaration

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -3326,10 +3326,16 @@ Some examples:
 Mmmm  => []
 Super => [2,4]
 Apple => [1,5]
-YoMama -> [1,2,4,6]
 NOTES
 Vowels in this context refers to: a e i o u y (including upper case)
 This is indexed from [1..n] (not zero indexed!)
 
 */
 
+function vowelIndices(word){
+
+}
+
+console.log(vowelIndices("amazing"), [1, 3, 5])
+console.log(vowelIndices("sun"), [2])
+console.log(vowelIndices("mmm"), [])
