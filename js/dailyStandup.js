c976231dc1e3c63d6852a4103ab6823df88f2d2c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-01-14 23:33:03 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -3629,3 +3629,21 @@ function pattern(n){
 console.log(pattern(3), "1\n1*2\n1**3")
 console.log(pattern(7),"1\n1*2\n1**3\n1***4\n1****5\n1*****6\n1******7");
 console.log(pattern(20),"1\n1*2\n1**3\n1***4\n1****5\n1*****6\n1******7\n1*******8\n1********9\n1*********10\n1**********11\n1***********12\n1************13\n1*************14\n1**************15\n1***************16\n1****************17\n1*****************18\n1******************19\n1*******************20")
+
+
+/*
+
+Jan 14, 2021
+
+Description:
+Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single 
+space, without leading/trailing spaces.
+
+Examples
+remove("Hi!") === ""
+remove("Hi! Hi!") === ""
+remove("Hi! Hi! Hi!") === ""
+remove("Hi Hi! Hi!") === "Hi"
+remove("Hi! !Hi Hi!") === ""
+remove("Hi! Hi!! Hi!") === "Hi!!"
+remove("Hi! !Hi! Hi!") === "!Hi!"
