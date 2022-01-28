f6ff5b654d1a057f03009778551a25764dacc302
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-01-28 00:33:12 -0800

    fixed function declaration title

--- js/coltSteel.js
+++ js/coltSteel.js
@@ -657,7 +657,7 @@ function bubbleSort(arr){
 
                                 /// Selection Sort ///
 
-function selectionSwap(arr){
+function selectionSort(arr){
   for(let i = 0; i < arr.length; i++){
     let lowest = i
     for(let j = i+1; j < arr.length; j++){
