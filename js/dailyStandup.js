b490b757de590a8236c9166b7713bda669c78716
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-02-10 22:13:24 -0800

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4201,3 +4201,22 @@ function switch1And0(n){
 console.log(switch1And0('11010'), '00101')
 console.log(switch1And0('00011'), '11100')
 console.log(switch1And0('0011100'), '1100011')
+
+
+/*
+
+Feb 10, 2022
+
+In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know 
+the rules, here they are:
+
+years divisible by 4 are leap years
+but years divisible by 100 are not leap years
+but years divisible by 400 are leap years
+Additional Notes:
+
+Only valid years (positive integers) will be tested, so you don't have to validate them
+Examples can be found in the test fixture.
+
+*/
+
