b052bde0cf4c4ff5320fc92a78928939b0be7212
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-03-04 22:10:39 -0800

    added solution

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4435,3 +4435,16 @@ unluckyDays
 (1986) == 1
 
 */
+
+function unluckyDays(year){
+  let unlucky = 0;
+  for (var i = 0; i < 12; i++) {
+    if(new Date(year, i, 13).getDay() === 5){
+      unlucky++;
+    }
+  }
+  return unlucky;
+}
+
+console.log()
+console.log()
