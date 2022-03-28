097f0d859447df3694aa16a531fdfa59fcb9fa67
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-03-27 22:27:14 -0700

    solution refactored

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4478,18 +4478,10 @@ Note: If the number is a multiple of both 3 and 5, only count it once.
 function solution(number){
   let total = 0
   
-  if(number < 0){
-    return 0
-  }
-  
   for(let i = 0; i < number; i++){
     if(i % 3 === 0 || i % 5 === 0 ){
-      total += i
-    } else if(i % 3 === 0){
-      total += i
-    } else if (i % 5 === 0){
-      total += i
-    }
+   total += 0
+   }
   }
   return total
 }
