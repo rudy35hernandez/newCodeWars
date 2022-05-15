427bd79a034f9c8da26ca8c385690ad77828098c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-05-14 23:28:53 -0700

    added solution

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4586,6 +4586,10 @@ n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 
 */
 
-function powerOfTwo(n){
-  
-}
+function powersOfTwo(n){
+  let arr = []
+  for(let i = 0; i <= n; i++){
+    arr.push(2**i)
+  }
+    return arr
+    }
