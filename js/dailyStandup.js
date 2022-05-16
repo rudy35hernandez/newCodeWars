4b717cb4f74e909f54d48913c48b05731067b5b1
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-05-15 23:36:05 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4593,3 +4593,29 @@ function powersOfTwo(n){
   }
     return arr
     }
+
+    
+    
+/*
+
+July 8, 2022
+
+Return an integer with the length of the consecutive vowels found.
+
+This is better explained with a couple of examples:
+
+You are given the string "agrtertyfikfmroyrntbvsukldkfa". The logic is that you start from the 'a' and make your 
+way right. The next vowel is an 'e' and it is the consecutive vowel, then 'i' and so forth until you get to 'u'. 
+If you keep moving right you find 'a' which happens to be the consecutive vowel. Return 6.
+This is a slightly trickier example: you are given the string "erfaiekjudhyfimngukduo". As you move right you 
+ignore all vowels until you get to the 'a', then ignore the rest until you get to the 'e', which is the consecutive 
+vowel, and so forth until you get to the 'o'. Return 4.
+
+Note
+For this kata, the vowels are 'a', 'e', 'i', 'o', 'u', in that order. 'y' is not considered a vowel in this kata.
+
+Good luck!
+
+*/
+
+
