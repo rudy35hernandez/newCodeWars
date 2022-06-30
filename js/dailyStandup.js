da517392240869ff5c7309d79936e0663ba8de4e
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-06-30 11:43:54 -0700

    added pseudo code

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4753,3 +4753,5 @@ assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42);
 assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41); 
 
 */
+
+// Added them all up using reduce method. Also  use the Number built in method to turn strings into numbers
