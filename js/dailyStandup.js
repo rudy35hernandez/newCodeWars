c40b32fd8dd09f8e9fb599fed56e08bd0870e593
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-06-30 11:43:10 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4731,10 +4731,25 @@ domainName("https://www.cnet.com") == "cnet
 
 function printDomain(url){
   url = url.replace("https://", "")
-  url = url.replace("http:")
+  url = url.replace("http://", "")
+  url = url.replace("www.", "")
+  return url.split(".")[0]
 
 }
 
 console.log(printDomain("https://www.sup.com"))
 console.log(printDomain("www.github.com/rudy35hernandez"))
 
+
+
+/* New Challenge
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+assert.strictEqual(sumMix([9, 3, '7', '3']), 22);
+assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42); 
+assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41); 
+
+*/
