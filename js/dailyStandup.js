62721734905eab7d96d01bc727bb8d93f3b17d40
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-07-03 08:57:48 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4762,3 +4762,21 @@ function addthemup(arr){
 
 console.log(addthemup([1, 4, "5", 22, "1"]))
 console.log(addthemup)([4, 5, "11", "3"])
+
+
+
+/* New Challenge
+
+It's the academic year's end, fateful moment of your school report. The averages must be calculated. 
+All the students come to you and entreat you to calculate their average for them. Easy ! You just need to 
+write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+
+getAverage([2,2,2,2]),2)
+getAverage([1,2,3,4,5,]),3);
+getAverage([1,1,1,1,1,1,1,2]),1)
+
+*/
