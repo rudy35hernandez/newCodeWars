13ccd4730da2fb4c5df49f8c68af20299b48910d
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-07-04 08:55:16 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4790,3 +4790,21 @@ function avgScore(arr){
 
 console.log(avgScore([1, 1, 1, 1, 1, 2]), 1)
 console.log(avgScore([1,2,3,4,5,]),3)
+
+
+
+/* New Challenge
+
+You will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task 
+will be to return the sum of the numbers that occur only once.
+
+For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every 
+other number occurs twice.
+
+repeats([4,5,7,5,4,8]),15)
+repeats([9, 10, 19, 13, 19, 13]),19)
+repeats([16, 0, 11, 4, 8, 16, 0, 11]),12)
+repeats([5, 17, 18, 11, 13, 18, 11, 13]),22)
+repeats([5, 10, 19, 13, 10, 13]),24)
+
+*/
