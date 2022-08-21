0b7b02eccb9928333fb5b5c67e5d524fe4cecc59
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-08-21 08:39:10 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4656,3 +4656,21 @@ Testing: [1, 0, 1, 1] ==> 11
 However, the arrays can have varying lengths, not just limited to 4.
 
 */
+
+
+/* 
+
+Write function which takes a string and make an acronym of it.
+
+Rule of making acronym in this kata:
+
+split string to words by space char
+take every first letter from word in given string
+uppercase it
+join them toghether
+Eg:
+
+Code wars -> C, w -> C W -> CW
+Note: There will be at least two words in the given string!
+
+*/
