1a0c6f171879bf909b05b3a3428108e4430d6a4d
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-11-03 08:59:11 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4932,3 +4932,20 @@ Examples
 /// Although I have not done this in a while, what I would do is create an object that keeps count of each 
 /// time a letter appears. After creating the hash map, I would then use a for loop to check which key has a value
 /// greater than 1 and return the ), if it appears once, I would return (.
+
+
+
+
+
+/* Given a string, return a new string that has transformed based on the input:
+
+Change case of every character, ie. lower case to upper case, upper case to lower case.
+Reverse the order of words from the input.
+Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+For example:
+
+"Example Input" ==> "iNPUT eXAMPLE"
+You may assume the input only contain English alphabet and spaces.
+
+*/
