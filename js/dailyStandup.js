d3d0c5ad3c8c8c6afea021aa1fa7c23ceea97c1c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-11-05 11:59:30 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -4972,3 +4972,22 @@ function reverseCaseAndWord(str){
   .reverse()
   .join(" ")
 }
+
+
+
+/* New Challenge
+
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final 
+character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the 
+given string.
+
+All inputs will be valid.
+
+last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']
+last('take me to semynak'), ['take', 'me', 'semynak', 'to'] 
+
+*/ 
+
