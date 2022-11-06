32ab7968b1d480b68f7386c810b8ec9d105c79bd
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-11-06 11:41:11 -0800

    added function declaration

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5024,3 +5024,7 @@ findDeletedNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), 5, 'Deletion')
 findDeletedNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]), 0, 'No deletion')
 
 */
+
+function findMissingNumber(arr){
+  
+}
