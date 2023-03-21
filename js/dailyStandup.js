80019bfe694574afe89f7f7e1634d4e71a03f6ad
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-03-20 22:28:39 -0700

    added functions to class to get values

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5169,4 +5169,23 @@ class Block {
   constructor(data) {
     this.data = data
   }
+  getWidth(){
+    return this.data[0]
+  }
+
+  getLength(){
+    return this.data[1]
+  }
+
+  getHeight(){
+    return this.data[2]
+  }
+
+  getVolume(){
+    return this.data.reduce((currVal, num) => currVal * num, 1)
+  }
+
+  getSurfaceArea(){
+    return 2 * ((this.data[2] * this.data[0]) + (this.data[2] * this.data[1]) + (this.data[1] * this.data[0]))
+  }
 }
