4ff7b0aa4d9b0ec2e81e53eb3ee3681920a0ba42
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-03-20 22:10:57 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5144,3 +5144,24 @@ function findOutliers(int){
   return even.length === 1 ? even[0] : odds[0]
 }
 
+
+/* 
+Write a class Block that creates a block (Duh..)
+
+Requirements
+The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+
+Define these methods:
+
+`getWidth()` return the width of the `Block`
+
+`getLength()` return the length of the `Block`
+
+`getHeight()` return the height of the `Block`
+
+`getVolume()` return the volume of the `Block`
+
+`getSurfaceArea()` return the surface area of the `Block`
+
+*/
+
