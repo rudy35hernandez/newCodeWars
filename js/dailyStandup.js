fb90baa7d038978de9d9c8853591fa24b3d25e2f
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-03-28 22:30:17 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5381,3 +5381,31 @@ Examples
 /// Next I need to join the letters again, then split by word, map each word, if its after index one, I need to
 /// split that word by each character, and index 0 will get a toUpperCase() and the rest stay the same.
 /// Lastly I will join the letters back up, join the words, split them by word (" ") then join("")
+
+function toCamelCase(str){
+  const wordArr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  
+  if(!str){
+    return ""
+  }
+  
+  const onlyLettersStr = str.split("").map((letter, index, arr) => wordArr.includes(letter) ? letter : " ").join("")
+  const camel = onlyLettersStr.split(" ")
+                .map((word, index, arr) => index === 0 ? word : 
+                word.split("").map((letter,index) => index === 0 ? letter.toUpperCase() : letter).join("")).join(" ")
+  return camel.split(" ").join("")
+
+  /*
+
+  You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too 
+  early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its 
+  citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of 
+  one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single 
+  block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a 
+  function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want 
+  to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', 
+or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+*/
