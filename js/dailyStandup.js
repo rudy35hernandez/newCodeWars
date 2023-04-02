15dcbcf404074f825ce6f7e131c87e4b785be83c
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-04-02 00:37:41 -0700

    added funtion declaration and two test cases

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5465,5 +5465,11 @@ function isValidWalk(walk) {
   punctuation.
 
   */
- 
 
+
+function isPangram(string){
+  //...
+}
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog"), true)
+console.log(isPangram("This is not a pangram."), false)
