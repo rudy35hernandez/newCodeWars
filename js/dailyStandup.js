c882dce5e0bed01b8581e1dca0d92613cb51cd15
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-04-01 23:16:28 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5432,3 +5432,14 @@ function isValidWalk(walk) {
       return ns === 0 && ew === 0
 
   }
+
+  /* 
+
+  New Challenge
+
+  Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other 
+  elements.
+
+  moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+  */
