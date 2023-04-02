f4c3816813474514d0f24b36a65087fe9495f58b
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-04-02 00:35:47 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5455,3 +5455,15 @@ function isValidWalk(walk) {
   }
 
   console.log(moveZerosA([1,2,0,1,0,1,0,3,0,1]), [1, 2, 1, 1, 3, 1, 0, 0, 0, 0])
+
+  /*
+  A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the 
+  sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least 
+  once (case is irrelevant).
+
+  Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and 
+  punctuation.
+
+  */
+ 
+
