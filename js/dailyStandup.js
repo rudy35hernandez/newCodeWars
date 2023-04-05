7f19b41017a3c7a32611d979471fc4b51423065a
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-04-04 23:29:23 -0700

    added new challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5496,3 +5496,15 @@ function isPanagram2(str){
   const lowerStr = str.toLowerCase()
   return alph.split("").every(letter => lowerStr.includes(letter))
 }
+
+/*
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any 
+elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+*/
