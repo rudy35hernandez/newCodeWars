9519dc920104ccbc76fcc05f671c60530fdc1611
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-04-06 00:56:27 -0700

    added solution

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5543,3 +5543,7 @@ Good luck and try to be as creative as possible!
 */
 
 // Not sure what the challenge is asking us to do 
+
+  const helloWorld = () => {
+    return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33);
+  }
