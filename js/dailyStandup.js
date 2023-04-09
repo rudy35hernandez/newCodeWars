b6d578f6ae4daf95c45e206c66b307d518492fc4
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-04-08 23:15:23 -0700

    added challenge called digital root

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5547,3 +5547,19 @@ Good luck and try to be as creative as possible!
   const helloWorld = () => {
     return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33);
   }
+
+
+  /*
+  Digital root is the recursive sum of all the digits in a number.
+
+  Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way 
+  until a single-digit number is produced. The input will be a non-negative integer.
+
+  Examples
+      16  -->  1 + 6 = 7
+    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+  132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+  493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+*/
+
