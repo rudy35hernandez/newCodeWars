afb1615e1cef63c7260c628cb4679f9b5525e0db
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2023-04-27 23:05:15 -0700

    added challenge

--- js/dailyStandup.js
+++ js/dailyStandup.js
@@ -5580,3 +5580,19 @@ function digitalRoot(num){
   }
   
 }
+
+/*
+
+Well met with Fibonacci bigger brother, AKA Tribonacci.
+
+As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) 
+numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native 
+Italian speakers trying to pronounce it :(
+
+So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this 
+sequence:
+
+[1, 1 ,1, 3, 5, 9, 17, 31, ...]
+
+*/
+
